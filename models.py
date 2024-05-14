6803e2a51b191d10deda642e2393aa101baea9b0
Brigitte <114067290+brishar0n@users.noreply.github.com>
2024-05-14 09:40:55 +0700

    Update models.py

--- models.py
+++ models.py
@@ -2,23 +2,25 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from database import Base
-from sqlalchemy.dialects.postgresql import UUID
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    profileURL = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    password = Column(String)
+    hashed_password = Column(String)
+    username = Column(String)
 
-    todos = relationship("TodoItem", back_populates="owner")
+    todos = relationship("Todo", back_populates="owner")
 
-class TodoItem(Base):
+
+class Todo(Base):
     __tablename__ = "todos"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
-    task = Column(String, index=True)
-    completed = Column(Boolean, default=False)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    completed = Column(Boolean)
+    isEditing = Column(Boolean)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    user = relationship("User", back_populates="todo")
+    owner = relationship("User", back_populates="todos")
